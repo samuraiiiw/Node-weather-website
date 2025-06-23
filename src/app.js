66bd4db2cf2de0f3302d4e5d81062eeef7173ab6
unknown <aleksapavlovic9999@gmail.com>
2025-06-23 19:21:19 +0200

    Ispravljena greska u kucanju

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
 const app = express()
-const port = proccess.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 // Define paths for Express config
